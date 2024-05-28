2710d5f7cd1bc429df0eab752c3034e9399167b1
sheee37 <sheee37@naver.com>
2024-05-28 17:18:15 +0900

    0528_Map_제작중

---
+++ Source/victroyShip/Private/YSH/woodplank.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "YSH/woodplank.h"
+
+// Sets default values
+Awoodplank::Awoodplank()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void Awoodplank::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Awoodplank::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
