ecfd34b2fda2f2ff58904a9065a030cfd2bbc0f9
lsh210726 <136397554+lsh210726@users.noreply.github.com>
2024-06-07 12:26:25 +0900

    add neet

---
+++ Source/victroyShip/Public/LSH/NPC/NPC_Neet.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PKH/Interface/HourUpdate.h"
+#include "PKH/NPC/NPCBase.h"
+#include "NPC_Neet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VICTROYSHIP_API ANPC_Neet : public ANPCBase, public IHourUpdate
+{
+	GENERATED_BODY()
+	
+public:
+	ANPC_Neet();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void DoJob() override;
+
+	// Locations
+protected:
+	UPROPERTY(EditAnywhere, Category = "Locations")
+	FVector HillLoc;
+
+	UPROPERTY(EditAnywhere, Category = "Locations")
+	FVector ParkLoc;
+
+	// Interface
+public:
+	virtual void OnHourUpdated(int32 NewHour) override;
+};
