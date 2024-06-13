18a728eb32d1fc2cb1673c2ffcb4001d4fb38dd0
kyungho Park <99066633+pkh0308@users.noreply.github.com>
2024-06-13 20:39:59 +0900

    24.06.13 - NPC 초상화 대응

--- Source/victroyShip/Private/PKH/NPC/NPC_Programmer.cpp
+++ Source/victroyShip/Private/PKH/NPC/NPC_Programmer.cpp
@@ -3,12 +3,8 @@
 
 #include "PKH/NPC/NPC_Programmer.h"
 
-#include "Components/AudioComponent.h"
-#include "Components/SlateWrapperTypes.h"
-#include "Kismet/GameplayStatics.h"
 #include "PKH/Animation/NPCAnimInstance.h"
 #include "PKH/NPC/NPCController.h"
-#include "PKH/UI/EmotionUIWidget.h"
 
 #define HOUR_GO_WORK 9
 #define HOUR_GO_PARK 15
@@ -18,13 +14,15 @@ ANPC_Programmer::ANPC_Programmer()
 {
 	NPCType = ENPCType::Programmer;
 
-	HomeLoc = FVector(-4310, 4410, 985);
+	HomeLoc = FVector(-4372, 4411, 983);
 	WorkLoc = FVector(1480, 3340, 631); 
 	ParkLoc = FVector(-640, 2680, 520);
 
 	WorkRotation = FRotator(0, 200, 0);
 
 	SitDistance = -50.0f;
+
+	PreferItemName = TEXT("Cauliflower,EggPlant");
 
 	// Mesh
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshRef(TEXT("/Script/Engine.SkeletalMesh'/Game/Scanned3DPeoplePack/RP_Character/rp_claudia_rigged_002_ue4/rp_claudia_rigged_002_ue4.rp_claudia_rigged_002_ue4'"));
@@ -64,6 +62,58 @@ ANPC_Programmer::ANPC_Programmer()
 	if (LaptopClassRef.Class)
 	{
 		LaptopClass = LaptopClassRef.Class;
+	}
+
+	// Portraits
+	// Joy
+	for (int i = 1; i < 5; ++i)
+	{
+		FString RefText = FString::Printf(TEXT("/Script/Engine.Texture2D'/Game/PKH/Portaraits/Programmer/Programmer_Joy%d.Programmer_Joy%d'"), i, i);
+		static ConstructorHelpers::FObjectFinder<UTexture2D> Portrait_JoyRef(*RefText);
+		if (Portrait_JoyRef.Object)
+		{
+			Portraits_Joy.Add(Portrait_JoyRef.Object);
+		}
+	}
+	// Surprise
+	for (int i = 1; i < 5; ++i)
+	{
+		FString RefText = FString::Printf(TEXT("/Script/Engine.Texture2D'/Game/PKH/Portaraits/Programmer/Programmer_Surprise%d.Programmer_Surprise%d'"), i, i);
+		static ConstructorHelpers::FObjectFinder<UTexture2D> Portrait_SurpriseRef(*RefText);
+		if (Portrait_SurpriseRef.Object)
+		{
+			Portraits_Surprise.Add(Portrait_SurpriseRef.Object);
+		}
+	}
+	// Sad
+	for (int i = 1; i < 5; ++i)
+	{
+		FString RefText = FString::Printf(TEXT("/Script/Engine.Texture2D'/Game/PKH/Portaraits/Programmer/Programmer_Sad%d.Programmer_Sad%d'"), i, i);
+		static ConstructorHelpers::FObjectFinder<UTexture2D> Portrait_SadRef(*RefText);
+		if (Portrait_SadRef.Object)
+		{
+			Portraits_Sadness.Add(Portrait_SadRef.Object);
+		}
+	}
+	// Anger
+	for (int i = 1; i < 5; ++i)
+	{
+		FString RefText = FString::Printf(TEXT("/Script/Engine.Texture2D'/Game/PKH/Portaraits/Programmer/Programmer_Anger%d.Programmer_Anger%d'"), i, i);
+		static ConstructorHelpers::FObjectFinder<UTexture2D> Portrait_AngerRef(*RefText);
+		if (Portrait_AngerRef.Object)
+		{
+			Portraits_Anger.Add(Portrait_AngerRef.Object);
+		}
+	}
+	// Default
+	for (int i = 1; i < 5; ++i)
+	{
+		FString RefText = FString::Printf(TEXT("/Script/Engine.Texture2D'/Game/PKH/Portaraits/Programmer/Programmer_Default%d.Programmer_Default%d'"), i, i);
+		static ConstructorHelpers::FObjectFinder<UTexture2D> Portrait_DefaultRef(*RefText);
+		if (Portrait_DefaultRef.Object)
+		{
+			Portraits_Default.Add(Portrait_DefaultRef.Object);
+		}
 	}
 }
 
@@ -126,38 +176,6 @@ void ANPC_Programmer::StandUp()
 	SetActorLocation(TargetLoc);
 }
 
-void ANPC_Programmer::StartConversation()
-{
-	if (ACharacter* Player = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0))
-	{
-		// Turn to player
-		FVector DirectionVec = (Player->GetActorLocation() - GetActorLocation()).GetSafeNormal();
-		DirectionVec.Z = 0;
-		const FRotator TargetRot = DirectionVec.ToOrientationRotator();
-		SetActorRotation(TargetRot);
-
-		NPCController->StartConversation();
-		if(false == NPCController->GetIsWorking())
-		{
-			AnimInstance->PlayMontage_Conv();
-		}
-		EmotionUI->SetVisibility(ESlateVisibility::Hidden);
-	}
-}
-
-void ANPC_Programmer::EndConversation()
-{
-	EmotionUI->SetVisibility(ESlateVisibility::Hidden);
-	NPCController->EndConversation();
-	if (false == NPCController->GetIsWorking())
-	{
-		AnimInstance->StopAllMontages(0);
-	}
-	SetNPCWalk();
-
-	OnConversationEnd();
-}
-
 void ANPC_Programmer::OnConversationEnd()
 {
 	if(NPCController->GetIsWorking() && MyGameMode->GetHour() >= HOUR_GO_PARK)
@@ -170,56 +188,9 @@ void ANPC_Programmer::OnConversationEnd()
 	}
 }
 
-void ANPC_Programmer::PlayEmotion(bool IsUIOnly)
+bool ANPC_Programmer::CanRotateInWorking()
 {
-	if (CurEmotion.IsEmpty())
-	{
-		return;
-	}
-
-	EmotionUI->SetEmotion(CurEmotion);
-	EmotionUI->SetVisibility(ESlateVisibility::Visible);
-	if (IsUIOnly)
-	{
-		if (Sfx_Notice)
-		{
-			SfxComp->SetSound(Sfx_Notice);
-			SfxComp->Play();
-		}
-	}
-	else
-	{
-		if (false == NPCController->GetIsWorking())
-		{
-			AnimInstance->PlayMontage_Emotion(CurEmotion);
-		}
-
-		if (CurEmotion == "joy" && IsValid(Sfx_Joy))
-		{
-			SfxComp->SetSound(Sfx_Joy);
-		}
-		else if (CurEmotion == "anger" && IsValid(Sfx_Anger))
-		{
-			SfxComp->SetSound(Sfx_Anger);
-		}
-		else if (CurEmotion == "sadness" && IsValid(Sfx_Sad))
-		{
-			SfxComp->SetSound(Sfx_Sad);
-		}
-		else if (CurEmotion == "surprise" && IsValid(Sfx_Surprise))
-		{
-			SfxComp->SetSound(Sfx_Surprise);
-		}
-		else if (IsValid(Sfx_Indiff))
-		{
-			SfxComp->SetSound(Sfx_Indiff);
-		}
-
-		if (SfxComp->Sound)
-		{
-			SfxComp->Play();
-		}
-	}
+	return false;
 }
 #pragma endregion
 
